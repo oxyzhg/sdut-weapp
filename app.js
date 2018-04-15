b0e5d03b0579db6f9d9bb86b6c6bec98ed491706
syessn <syessn@gmail.com>
2018-04-15 11:27:04 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+App({
+  onLaunch() {
+    this.getWxInfo();
+  },
+  onShow() {
+    this.getWxInfo();
+  },
+  getWxInfo() {
+    wx.login({
+      success: (res) => {
+        let { code } = res;
+        wx.getUserInfo({
+          success: (res) => {
+            let { userInfo } = res;
+            wx.setStorageSync('userInfo', userInfo);
+          },
+          fail: (err) => {
+            this.showToast('信息获取失败', 'warn');
+          }
+        });
+      },
+      fail: (err) => {
+        this.showToast('用户登录失败', 'warn');
+      }
+    });
+  },
+  showToast(title = '加载中', icon = 'loading', duration = 2000, mask = true) {
+    wx.showToast({
+      title,
+      icon,
+      duration,
+      mask
+    });
+  },
+  globalData: {
+    collegeList: [
+      '机械工程学院',
+      '交通与车辆工程学院',
+      '农业工程与食品科学学院',
+      '电气与电子工程学院',
+      '计算机科学与技术学院',
+      '化学化工学院',
+      '建筑工程学院',
+      '资源与环境工程学院',
+      '材料科学与工程学院',
+      '生命科学学院',
+      '数学与统计学院',
+      '物理与光电工程学院',
+      '经济学院',
+      '管理学院',
+      '文学与新闻传播学院',
+      '外国语学院',
+      '法学院',
+      '马克思主义学院',
+      '美术学院',
+      '音乐学院',
+      '体育学院',
+      '国防教育学院',
+      '鲁泰纺织服装学院'
+    ]
+  }
+});
---
+++ pages/feature/index/index.js
@@ -0,0 +1,133 @@
+// pages/feature/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    features: [
+      {
+        id: 1,
+        name: '绩点查询',
+        name_en: 'jidian',
+        iconPath: '/images/tab/icon_feature.png',
+        navigateUrl: ''
+      },
+      {
+        id: 2,
+        name: '成绩查询',
+        name_en: 'jidian',
+        iconPath: '/images/tab/icon_feature.png',
+        navigateUrl: ''
+      },
+      {
+        id: 3,
+        name: '综测查询',
+        name_en: 'jidian',
+        iconPath: '/images/tab/icon_feature.png',
+        navigateUrl: ''
+      },
+      {
+        id: 4,
+        name: '荣誉称号',
+        name_en: 'jidian',
+        iconPath: '/images/tab/icon_feature.png',
+        navigateUrl: ''
+      },
+      {
+        id: 5,
+        name: '考试地点查询',
+        name_en: 'jidian',
+        iconPath: '/images/tab/icon_feature.png',
+        navigateUrl: ''
+      },
+      {
+        id: 6,
+        name: '宿舍卫生查询',
+        name_en: 'jidian',
+        iconPath: '/images/tab/icon_feature.png',
+        navigateUrl: ''
+      },
+      {
+        id: 7,
+        name: '用电查询',
+        name_en: 'jidian',
+        iconPath: '/images/tab/icon_feature.png',
+        navigateUrl: ''
+      },
+      {
+        id: 8,
+        name: '校历',
+        name_en: 'jidian',
+        iconPath: '/images/tab/icon_feature.png',
+        navigateUrl: ''
+      },
+      {
+        id: 9,
+        name: '失物招领',
+        name_en: 'jidian',
+        iconPath: '/images/tab/icon_feature.png',
+        navigateUrl: ''
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: '山东理工大学学生服务',
+      path: '/pages/feature/index/index'
+    }
+  }
+})
